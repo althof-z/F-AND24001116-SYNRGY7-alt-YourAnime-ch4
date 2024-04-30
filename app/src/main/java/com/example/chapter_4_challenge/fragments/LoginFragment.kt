304192a2698adb02bc386a956bf994603ef9938c
AlthofZ <althof.edu@gmail.com>
2024-04-30 22:31:24 +0700

    Login and Register Fragment

---
+++ app/src/main/java/com/example/chapter_4_challenge/fragments/LoginFragment.kt
@@ -0,0 +1,41 @@
+package com.example.chapter_4_challenge.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.chapter_4_challenge.R
+import com.example.chapter_4_challenge.databinding.FragmentLoginBinding
+import com.example.chapter_4_challenge.databinding.FragmentRegisterBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+
+    private lateinit var viewBinding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        return FragmentLoginBinding.inflate(layoutInflater, container, false).also {
+            viewBinding = it
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewBinding.tvGotoRegister.setOnClickListener {
+            it.findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/chapter_4_challenge/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/chapter_4_challenge/fragments/RegisterFragment.kt
@@ -6,19 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
+import com.example.chapter_4_challenge.databinding.FragmentRegisterBinding
 import com.example.chapter_4_challenge.R
 import com.example.chapter_4_challenge.databinding.FragmentFirstBinding
 
-class  FirstFragment : Fragment() {
 
-    private lateinit var viewBinding: FragmentFirstBinding
+/**
+ * A simple [Fragment] subclass.
+ * Use the [RegisterFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RegisterFragment : Fragment() {
+
+    private lateinit var viewBinding: FragmentRegisterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        return FragmentFirstBinding.inflate(layoutInflater, container, false).also {
+        return FragmentRegisterBinding.inflate(layoutInflater, container, false).also {
             viewBinding = it
         }.root
     }
@@ -26,18 +33,10 @@ class  FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewBinding.btnGo.setOnClickListener{
-            it.findNavController().navigate(R.id.action_firstFragment_to_animeGenreFragment)
-
-        }
-
-        viewBinding.btnRegister.setOnClickListener {
-            it.findNavController().navigate(R.id.action_firstFragment_to_registerFragment)
-        }
-
-        viewBinding.btnLogin.setOnClickListener {
-            it.findNavController().navigate(R.id.action_firstFragment_to_loginFragment)
+        viewBinding.tvGotoLogin.setOnClickListener {
+            it.findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
     }
+
 
 }
